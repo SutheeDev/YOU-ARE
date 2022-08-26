6da6990ba675f228567f6350c217cb7c207241b7
SU-T <guitaristpeet@gmail.com>
2022-08-26 16:13:30 -0700

    add text animation

--- main.js
+++ main.js
@@ -1,11 +1,62 @@
-// const wordsList = ['awesome', 'beautiful', 'enough', 'loved', 'brilliant', 'unique', 'bold', 'impressive', 'bold'];
-// const words = document.querySelector('.words');
-
-// const wordsEl = wordsList.map((word) => {
-//     return `
-//         <span class="words">${word}</span>
-//     `;
-// });
-// document.addEventListener('DOMContentLoaded', () => {
-    
-// });
+function textMove (){
+    gsap.fromTo('.ab1', {
+        y: 100
+    }, {
+        y: 0,
+        duration: 0.3
+    }); 
+    gsap.fromTo('.bl1', {
+        y: -100
+    }, {
+        y: 0,
+        duration: 0.3
+    });
+    gsap.fromTo('.ab2', {
+        y: 200
+    }, {
+        y: 0,
+        duration: 0.3
+    }); 
+    gsap.fromTo('.bl2', {
+        y: -200
+    }, {
+        y: 0,
+        duration: 0.3
+    });
+    gsap.fromTo('.ab3', {
+        y: 300
+    }, {
+        y: 0,
+        duration: 0.3
+    });
+    gsap.fromTo('.bl3', {
+        y: -300
+    }, {
+        y: 0,
+        duration: 0.3
+    });
+    gsap.fromTo('.ab4', {
+        y: 400
+    }, {
+        y: 0,
+        duration: 0.3
+    });
+    gsap.fromTo('.bl4', {
+        y: -400
+    }, {
+        y: 0,
+        duration: 0.3
+    });
+    gsap.fromTo('.ab5', {
+        y: 500
+    }, {
+        y: 0,
+        duration: 0.3
+    });
+    gsap.fromTo('.bl5', {
+        y: -500
+    }, {
+        y: 0,
+        duration: 0.3
+    });
+};
